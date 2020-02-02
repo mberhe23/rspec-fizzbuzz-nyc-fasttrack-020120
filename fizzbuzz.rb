53d2ab59211e408067468bf5a8ee855d2c37d33b
Maryan W. Berhe <maryanberhe@gmail.com>
2020-02-02 20:36:26 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,26 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(int)
+  if int % 3 == 0 &&
+    "Fizz" 
+  end
+  
+  if int % 5 == 0 
+    "Buzz"
+  end
+  
+  if int % 15 == 0
+    "FizzBuzz" 
+    
+
+    
+  # if int !% 15,5,3 == 0 
+  end
+
+end
+
+# fizzbuzz(3) # => You should see a return of "Fizz"
+# fizzbuzz(5) # => You should see a return of "Buzz"
+# fizzbuzz(15) #=> You should see a return of "FizzBuzz"
+# fizzbuzz(4) #=> You should see a return of nil
+
