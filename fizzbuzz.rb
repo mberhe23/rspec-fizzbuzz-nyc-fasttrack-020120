50d9ef430314de2fe56a8030d9c4d412c0c89de9
Maryan W. Berhe <maryanberhe@gmail.com>
2020-02-02 20:40:52 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,9 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
+  
+  
+  
   if int % 3 == 0 
     "Fizz" 
   
@@ -9,8 +12,7 @@ def fizzbuzz(int)
     "Buzz"
   
   
-  else int % 15  == 0
-    "FizzBuzz" 
+  
     
 
     
